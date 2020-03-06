18b8ab4b62701d41e470f1fd734cd7bca1313fd9
Alesspop <46914352+Alesspop@users.noreply.github.com>
2020-03-05 18:30:12 -0600

    Analog clock hw assignment 1

---
+++ script.js
@@ -0,0 +1,24 @@
+alert(" You will now check the time with the digital clock.");
+
+const HOURHAND = document.querySelector("#hr");
+const MINUTEHAND = document.querySelector("#min");
+const SECONDHAND = document.querySelector("#sec");
+
+function runTheClock() {
+    var date = new Date();
+    console.log(date);
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+
+    let hrPosition = (hr*360/12)+(min*(360/60)/12);
+    let minPosition = (min*360/60)+(sec*(360/60)/60);
+    let secPosition = sec*360/60;
+
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runTheClock, 1000);
